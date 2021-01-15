7c008d2ca699aacead8c5f6ac86a46dea254b6cc
Ahmed <ahmedali.96@hotmail.com>
2021-01-14 19:40:30 -0500

    removed bloat

--- server/pool.js
+++ server/pool.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "***your pass***",
+  password: "@Mypasswallahi1",
   host: "localhost",
   port: 5432,
   database: "notedb",
